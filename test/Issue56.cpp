05ccf2b6b59673c51c282681273841fa0a571286
Benoit Blanchon <github@benoitblanchon.fr>
2015-03-07 16:11:56 +0100

    Test that we can use an Arduino String as a value of a JsonArray

--- test/Issue56.cpp
+++ test/Issue56.cpp
@@ -20,3 +20,17 @@ TEST(Issue56, UseStringAsValueOfAnObject) {
   o.printTo(json, sizeof(json));
   ASSERT_STREQ("{\"key\":\"value\"}", json);
 }
+
+TEST(Issue56, UseStringAsValueOfAnArray) {
+  DynamicJsonBuffer jsonBuffer;
+
+  JsonArray& a = jsonBuffer.createArray();
+  String s = "value";
+
+  a.add(s);
+  ASSERT_STREQ("value", a[0]);
+
+  char json[128];
+  a.printTo(json, sizeof(json));
+  ASSERT_STREQ("[\"value\"]", json);
+}
